2c3595f0aab3d1fe40c70c99d0a79a21f38080e3
minimalDev01 <sergiocn.dev@gmail.com>
2021-04-08 17:53:46 +0200

    Register with redirect added

--- Tweets/Modules/Welcome/RegisterViewController.swift
+++ Tweets/Modules/Welcome/RegisterViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import NotificationBannerSwift
+import Simple_Networking
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
     // MARK: - Outlets
@@ -43,13 +45,30 @@ class RegisterViewController: UIViewController {
             return
         }
         
-        guard let name = nameTextField.text, !name.isEmpty else {
+        guard let names = nameTextField.text, !names.isEmpty else {
             NotificationBanner(title: "Error", subtitle: "You must specify your full name", style: BannerStyle.warning).show()
             return
         }
         
-        performSegue(withIdentifier: "showHome", sender: nil)
+        // Create request
+        let request = RegisterRequest(email: email, password: password, names: names)
         
-        // Register here
+        // Initializating loader
+        SVProgressHUD.show()
+        
+        // Call net library to get the response. SNResultWithEntity return first the response (LoginResponse), and second the possible error (ErrorResponse)
+        SN.post(endpoint: Endpoints.register, model: request) { (response: SNResultWithEntity<LoginResponse, ErrorResponse>) in
+            // Dismissing loader
+            SVProgressHUD.dismiss()
+            
+            switch response {
+            case .success(let response):
+                self.performSegue(withIdentifier: "showHome", sender: nil)
+            case .error(let error):
+                NotificationBanner(title: "Error", subtitle: error.localizedDescription, style: .danger).show()
+            case .errorResult(let entity):
+                NotificationBanner(title: "Error", subtitle: entity.error, style: .warning).show()
+            }
+        }
     }
 }
