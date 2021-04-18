56bf869cd97c7a8b9f15a613f8ba81f9d7a9440c
minimalDev01 <sergiocn.dev@gmail.com>
2021-04-18 12:56:00 +0200

    Connection to firebase, testing app on iPhone

--- Tweets/Modules/Home/AddPostViewController.swift
+++ Tweets/Modules/Home/AddPostViewController.swift
@@ -14,6 +14,7 @@ class AddPostViewController: UIViewController {
     // MARK: -IBOutlet
     @IBOutlet weak var postTextView: UITextView!
     @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var previewImageView: UIImageView!
     
     // MARK: -IBActions
     @IBAction func addPostAction() {
@@ -23,6 +24,13 @@ class AddPostViewController: UIViewController {
     @IBAction func dismissAction() {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func openCameraAction() {
+        openCamera()
+    }
+    
+    // MARK: - Properties
+    private var imagePicker: UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +65,38 @@ class AddPostViewController: UIViewController {
             case .errorResult(let entity):
                 NotificationBanner(title: "Error", subtitle: entity.error, style: .warning).show()
             }
+        }
+    }
+    
+    private func openCamera() {
+        imagePicker = UIImagePickerController()
+        imagePicker?.sourceType = .camera
+        imagePicker?.cameraFlashMode = .off
+        imagePicker?.cameraCaptureMode = .photo
+        imagePicker?.allowsEditing = true
+        imagePicker?.delegate = self
+        
+        // Check if imagePicker exists in order to execute "present" descripted above, if not return
+        guard let imagePicker = imagePicker else {
+            return
+        }
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+// MARK: -UIImagePickerControllerDelegate
+extension AddPostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Close camera
+        imagePicker?.dismiss(animated: true, completion: nil)
+        
+        // Check if our posts contain an image
+        if info.keys.contains(.originalImage) {
+            previewImageView.isHidden = false
+            
+            // Getting the taken photo
+            previewImageView.image = info[.originalImage] as? UIImage
         }
     }
 }
